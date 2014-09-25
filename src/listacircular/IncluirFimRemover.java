08ce057176b6f6ff6b4c5d120868c5997a408a67
Odirlei B. Pereira <odirleibr@hotmail.com>
2014-09-25 11:27:09 -0400

    Arquivo modificado.

--- src/listacircular/IncluirFimRemover.java
+++ src/listacircular/IncluirFimRemover.java
@@ -1,18 +1,23 @@
 package listacircular;
 
 public class IncluirFimRemover implements ListaCircular {
-        private Celula inicio = null;
-        private Celula fim = null;
+
+    private Celula inicio = null;
+    private Celula fim = null;
+
     @Override
     public void indiceAtual(Mensagem mensagem) {
         Celula nova = new Celula(mensagem);
         if (inicio == null) {
             inicio = nova;
             fim = nova;
+            fim.setProxima(inicio);
         } else {
             fim.setProxima(nova);
             fim = nova;
+            fim.setProxima(inicio);
         }
+        System.out.println("Numero inserido no fim da lista!!");
     }
 
     @Override
@@ -36,7 +41,7 @@ public class IncluirFimRemover implements ListaCircular {
 
     @Override
     public void imprimirLista() {
-    
+
     }
-    
+
 }
